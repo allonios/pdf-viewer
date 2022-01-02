6dfed5ecc670c5c12d7f3a63b2f95b4963fc0d00
Allonios <feydi.allony@kuwaitnet.com>
2022-01-02 17:59:40 +0200

    feature: added CallbackProcessor

--- image_handlers/base.py
+++ image_handlers/base.py
@@ -2,9 +2,22 @@ import cv2
 
 
 class BaseImageHandler:
-    def __init__(self, processors, init_image=None):
+    def __init__(
+            self,
+            processors,
+            init_image=None,
+            registered_windows_names=("Original Image", "Result")
+    ):
         self.processors = processors
         self.current_image = init_image
+        self.registered_windows_names = list(registered_windows_names)
+        for window_name in self.registered_windows_names:
+            cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+
+    def register_windows_names(self, windows_names):
+        self.registered_windows_names.extend(windows_names)
+        for window_name in windows_names:
+            cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 
     def run_processors(self):
         for processor in self.processors:
@@ -12,6 +25,7 @@ class BaseImageHandler:
             self.current_image = processor(self.current_image)
 
     def run(self):
+        cv2.imshow("Original Image", self.current_image)
         self.run_processors()
-        cv2.imshow("Image", self.current_image)
+        cv2.imshow("Result", self.current_image)
         cv2.waitKey()
--- image_processors/CallbackProcessor.py
+++ image_processors/CallbackProcessor.py
@@ -1,6 +1,11 @@
 from image_processors.base import BaseProcessor
 
 
-class CallBackProcessor(BaseProcessor):
-    def __init__(self, image=None):
+class CallbackProcessor(BaseProcessor):
+    def __init__(self, callback, image=None):
         super().__init__(image)
+        self.callback = callback
+
+    def process_image(self, image=None):
+        self.image = super().process_image(image)
+        return self.callback(self.image)
